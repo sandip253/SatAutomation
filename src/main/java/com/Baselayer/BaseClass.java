1450dc5821dac2d1c25a740f008259c973e9d4a0
Admin <Admin@DESKTOP-8T6288U>
2023-04-09 12:57:53 +0530

    welcome to jungle

---
+++ src/main/java/com/Baselayer/BaseClass.java
@@ -0,0 +1,13 @@
+package com.Baselayer;
+
+public class BaseClass {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to jungle");
+		System.out.println("i am sandip");
+		System.out.println("from maharashta");
+		
+
+	}
+
+}
